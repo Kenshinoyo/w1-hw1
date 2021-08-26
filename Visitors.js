fca5b2d7006fb43b6048f6df94bacaf96240064f
Garrad Belle <garrad.d.belle@gmail.com>
2021-08-26 02:58:22 -0400

    flexbox manipulation

--- Visitors.js
+++ Visitors.js
@@ -3,9 +3,11 @@ import React from 'react';
 class Visitors extends React.Component {
     render() {
         return (
-            <div id = 'webVisit'>
-                <h2>Website Visitors</h2>
-                821
+            <div className='visitBox'>
+                <div id = 'webVisit' >
+                    <h2>Website Visitors</h2>
+                    821
+                </div>
             </div>
         )
     }
